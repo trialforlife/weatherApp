942d1b1722c4de6d70c19e70b605d346841d8c87
Andrew Kravchuk <trialforlife@gmail.com>
2023-03-18 21:25:13 +0200

    fixed type err

--- _redux/sagas/forecastsSaga/forecastsSaga.ts
+++ _redux/sagas/forecastsSaga/forecastsSaga.ts
@@ -1,34 +1,55 @@
 import axios from 'axios';
 import {all, call, put, takeLatest} from 'redux-saga/effects';
-import {IForecast} from '../../../models/IForecast';
+import {IForecastResponse, ICoord} from '../../../models/IForecast';
 import {
   fetchForecastsFailure,
   fetchForecastsSuccess,
 } from '../../actions/forecastsActions/forecastsActions';
 import {forecastTypes} from '../../Actiontypes/forecastsTypes';
 
-const getForecasts = () =>
-  axios.get<IForecast[]>('https://jsonplaceholder.typicode.com/todos');
+const locationVinnitsya: ICoord = {
+  lat: 49.22584448644553,
+  lon: 28.439236844179426,
+};
+
+const token: string = '';
+
+const endPoint =
+  'https://api.openweathermap.org/data/2.5/forecast?lat=' +
+  locationVinnitsya.lat +
+  '&lon=' +
+  locationVinnitsya.lon +
+  '&units=metric&appid=' +
+  token;
+
+const getForecasts = () => axios.get<IForecastResponse>(endPoint);
 
 function* fetchForecastsSaga() {
   try {
-    const response = yield call(getForecasts);
+    const {data} = yield call(getForecasts);
+    const {list} = data;
     yield put(
       fetchForecastsSuccess({
-        forecasts: response.data,
+        forecasts: list,
       }),
     );
   } catch (e) {
+    let errorMessage = 'Failed fetch posts';
+    if (e instanceof Error) {
+      errorMessage = e.message;
+    }
     yield put(
       fetchForecastsFailure({
-        error: e.message,
+        error: errorMessage,
       }),
     );
   }
 }
 
 function* forecastsSaga() {
-  yield all([takeLatest(forecastTypes.FETCH_FORECAST_REQUEST, fetchForecastsSaga)]);
+  yield all([
+    takeLatest(forecastTypes.FETCH_FORECAST_REQUEST, fetchForecastsSaga),
+  ]);
 }
 
 export default forecastsSaga;
